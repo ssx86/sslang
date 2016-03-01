a0c54cfb7e5a7a92d8388528f08450430dc66939
ssx86 <zss2366@gmail.com>
2016-03-01 20:03:18 +0800

    remove case output

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "-----------case1------------"
-cat ./test/case1.ss && make && ./sslang ./test/case1.ss
+make && ./sslang ./test/case1.ss
 echo "-----------case2------------"
-cat ./test/case2.ss && make && ./sslang ./test/case2.ss
+make && ./sslang ./test/case2.ss
