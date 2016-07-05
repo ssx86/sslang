66cd090d2f7fad9a3ddb9e05c8219b6b4ed5712f
ssx86 <zss2366@gmail.com>
2016-07-06 00:07:45 +0800

    test display

--- run_test.rb
+++ run_test.rb
@@ -17,9 +17,13 @@ Find.find('./test_cases') do |file|
     puts "[pass] #{basename}"
     $pass += 1
   else
-    puts "[fail] #{basename}"
+    puts "[\033[5mfail\033[0m] #{basename}"
     $fail += 1
   end
 end
 
-puts "test done, #{$pass + $fail} cases, #{$pass} passed, #{$fail} failed, #{$pass.to_f / ($pass + $fail) * 100}%."
+if $fail > 0 then
+  puts "\ntest done, #{$pass + $fail} cases, #{$pass} passed, \033[31m#{$fail} failed\033[0m, \033[33m#{$pass * 100 / ($pass + $fail)}%\033[0m."
+else
+  puts "\ntest done, #{$pass + $fail} cases, all passed."
+end
--- test_cases/assign.ss
+++ test_cases/assign.ss
@@ -1,6 +1,7 @@
 a = 10
 b = 30
 c = 50 + b / a
+c = c + 1
 print(c) 
 
 d = "hello"
